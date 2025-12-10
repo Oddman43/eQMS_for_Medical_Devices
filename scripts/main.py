97fd0236d17b564b5b2ab7dab1d66f1541ab1aae
oddman43 <albert.sevilleja@gmail.com>
2025-12-10 13:17:15 +0100

    added second doc

--- scripts/main.py
+++ scripts/main.py
@@ -1,11 +1,17 @@
 from doc_control_functions import create_new_document, write_new_doc
 from approvals import approve_document
+from revise_doc import revise_doc
 from config import db_path
 from datetime import datetime
 
 
 if __name__ == "__main__":
-    doc, ver = create_new_document("test", "SOP", 4, db_path)
+    doc, ver = create_new_document("test1", "SOP", 4, db_path)
     write_new_doc(doc, ver, db_path)
     approve_document("charlie_eng", 1, db_path)
     approve_document("alice_qa", 1, db_path, datetime.now().isoformat())
+    revise_doc("alice_qa", "SOP-001", db_path)
+    doc, ver = create_new_document("test2", "SOP", 4, db_path)
+    write_new_doc(doc, ver, db_path)
+    approve_document("charlie_eng", 2, db_path)
+    approve_document("alice_qa", 2, db_path, datetime.now().isoformat())
