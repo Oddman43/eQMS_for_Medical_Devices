109c066dced63f6f4979a1e24ef0e8c96ef87737
oddman43 <albert.sevilleja@gmail.com>
2025-12-16 18:01:44 +0100

    changed imports and implmeneted changes

--- scripts/main.py
+++ scripts/main.py
@@ -1,20 +1,43 @@
 from create_doc import create_new_document
-from approvals import approve_document
+from actions import doc_action
 from revise_doc import revise_doc
 from config import db_path
 from datetime import datetime
 
 
 if __name__ == "__main__":
-    create_new_document("test1", "SOP", "charlie_eng", db_path)
-    approve_document("charlie_eng", "SOP-001", db_path)
-    approve_document("alice_qa", "SOP-001", db_path, datetime.now().isoformat())
-    revise_doc("alice_qa", "SOP-001", db_path)
-    create_new_document("test2", "SOP", "charlie_eng", db_path)
-    approve_document("charlie_eng", "SOP-002", db_path)
-    approve_document("alice_qa", "SOP-002", db_path, datetime.now().isoformat())
-    approve_document("charlie_eng", "SOP-001", db_path)
-    approve_document("alice_qa", "SOP-001", db_path, datetime.now().isoformat())
-    revise_doc("alice_qa", "SOP-001", db_path)
-    approve_document("charlie_eng", "SOP-001", db_path)
-    approve_document("alice_qa", "SOP-001", db_path, datetime.now().isoformat())
+    create_new_document("test1", "SOP", "albert.sevilleja", db_path)
+    document_action = doc_action("APPROVE")
+    document_action("albert.sevilleja", "SOP-001", db_path)
+    document_action = doc_action("APPROVE")
+    document_action("quality.manager", "SOP-001", db_path, datetime.now().isoformat())
+    revise_doc("quality.manager", "SOP-001", db_path)
+    create_new_document("test2", "SOP", "albert.sevilleja", db_path)
+    document_action = doc_action("APPROVE")
+    document_action("albert.sevilleja", "SOP-002", db_path)
+    document_action = doc_action("APPROVE")
+    document_action("quality.manager", "SOP-002", db_path, datetime.now().isoformat())
+    document_action = doc_action("APPROVE")
+    document_action("albert.sevilleja", "SOP-001", db_path)
+    document_action = doc_action("APPROVE")
+    document_action("quality.manager", "SOP-001", db_path, datetime.now().isoformat())
+    revise_doc("quality.manager", "SOP-001", db_path)
+    document_action = doc_action("APPROVE")
+    document_action("albert.sevilleja", "SOP-001", db_path)
+    document_action = doc_action("REJECT")
+    document_action(
+        "quality.manager", "SOP-001", db_path, datetime.now().isoformat(), "misc"
+    )
+    document_action = doc_action("APPROVE")
+    document_action("albert.sevilleja", "SOP-001", db_path)
+    document_action = doc_action("APPROVE")
+    document_action("quality.manager", "SOP-001", db_path, datetime.now().isoformat())
+    revise_doc("quality.manager", "SOP-002", db_path)
+    create_new_document("test3", "WI", "albert.sevilleja", db_path)
+    document_action = doc_action("APPROVE")
+    document_action("albert.sevilleja", "WI-001", db_path)
+    document_action = doc_action("APPROVE")
+    document_action("quality.manager", "WI-001", db_path, datetime.now().isoformat())
+    revise_doc("quality.manager", "WI-001", db_path)
+    document_action = doc_action("APPROVE")
+    document_action("albert.sevilleja", "WI-001", db_path)
