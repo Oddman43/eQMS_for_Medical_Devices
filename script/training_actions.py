2caf9f98938af54c11c78725dc531201d27e7bae
oddman43 <albert.sevilleja@gmail.com>
2026-01-08 11:45:57 +0100

    created do_training fn to complete or fail training

--- script/training_actions.py
+++ script/training_actions.py
@@ -1,4 +1,28 @@
 from types import FunctionType
+from copy import deepcopy
+from datetime import datetime
+from training_class import Training
+from core_fn import get_training, audit_log_training, update_training
 
 
 def training_actions(action: str) -> FunctionType: ...
+
+
+def do_training(user_id: int, doc_num: str, score: int, db_path: str) -> None:
+    old_training_obj: Training = get_training(user_id, doc_num, db_path)
+    new_training_obj: Training = deepcopy(old_training_obj)
+    new_training_obj.score = score
+    if score > 70:
+        new_training_obj.status = "COMPLETED"
+        new_training_obj.completion_date = datetime.now()
+
+    else:
+        new_training_obj.status = "FAILED"
+    audit_log_training(
+        old_training_obj,
+        new_training_obj,
+        user_id,
+        new_training_obj.status,
+        db_path,
+    )
+    update_training(old_training_obj, new_training_obj, db_path)
